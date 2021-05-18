e9a280ebd42b9139cc7113655b7d1b190dda7cce
Dasacav3 <dscv3719@gmail.com>
2021-05-18 12:08:50 -0500

    Actualización de index

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 
     require_once "app/libraries/database.php";
     require_once "app/libraries/controller.php";
+    require_once "app/libraries/model.php";
     require_once "app/libraries/view.php";
     require_once "app/libraries/app.php";
+
+    require_once "app/controller/session.php";
 
     require_once "app/config/config.php";
 
     $app = new App;
+
+?>
