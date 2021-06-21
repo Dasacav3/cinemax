135600fb43a5b3c0a9cda3a3c03657812148c264
Dasacav3 <dscv3719@gmail.com>
2021-06-21 14:52:47 -0500

    Añadido password hash registro y login

--- app/models/loginmodel.php
+++ app/models/loginmodel.php
@@ -1,1 +1,36 @@
-<?php class LoginModel extends Model{public function __construct(){parent::__construct();}public function select($datos){try{$query=$this->db->connect()->prepare("SELECT * FROM usuario WHERE nombre_usuario = :nombre AND clave_usuario = :pass");$query->execute(['nombre'=>$datos['user_name'],'pass'=>$datos['password']]);$result=$query->fetch(PDO::FETCH_ASSOC);if($result['TIPO_USUARIO']=='Administrador'){try{$admin=$this->db->connect()->prepare("SELECT u.ID_USUARIO, u.NOMBRE_USUARIO, u.TIPO_USUARIO, a.ID_ADMIN, a.NOMBRE_ADMIN, a.APELLIDO_ADMIN FROM usuario as u INNER JOIN administrador as a ON u.ID_USUARIO = a.ID_USUARIO WHERE u.NOMBRE_USUARIO = :nombre AND u.CLAVE_USUARIO = :pass");$admin->execute(['nombre'=>$datos['user_name'],'pass'=>$datos['password']]);$data=$admin->fetch(PDO::FETCH_ASSOC);}catch(Exception $e){return $e->getMessage();}return $data;}else if($result['TIPO_USUARIO']=='Cliente'){try{$cliente=$this->db->connect()->prepare("SELECT u.ID_USUARIO, u.NOMBRE_USUARIO, u.TIPO_USUARIO, c.ID_CLIENTE, c.NOMBRE_CLIENTE, c.APELLIDO_CLIENTE FROM usuario as u INNER JOIN cliente as c ON u.ID_USUARIO = c.ID_USUARIO WHERE u.NOMBRE_USUARIO = :nombre AND u.CLAVE_USUARIO = :pass");$cliente->execute(['nombre'=>$datos['user_name'],'pass'=>$datos['password']]);$data=$cliente->fetch(PDO::FETCH_ASSOC);}catch(Exception $e){return $e->getMessage();}return $data;}}catch(Exception $e){return false;}}} ?>
+<?php class LoginModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function select($datos)
+    {
+        try {
+            $query = $this->db->connect()->prepare("SELECT * FROM usuario WHERE nombre_usuario = :nombre");
+            $query->execute(['nombre' => $datos['user_name']]);
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+            if ($result['TIPO_USUARIO'] == 'Administrador' and password_verify($datos['password'], $result['CLAVE_USUARIO'])) {
+                try {
+                    $admin = $this->db->connect()->prepare("SELECT u.ID_USUARIO, u.NOMBRE_USUARIO, u.TIPO_USUARIO, a.ID_ADMIN, a.NOMBRE_ADMIN, a.APELLIDO_ADMIN, u.CLAVE_USUARIO FROM usuario as u INNER JOIN administrador as a ON u.ID_USUARIO = a.ID_USUARIO WHERE u.NOMBRE_USUARIO = :nombre");
+                    $admin->execute(['nombre' => $datos['user_name']]);
+                    $data = $admin->fetch(PDO::FETCH_ASSOC);
+                } catch (Exception $e) {
+                    return $e->getMessage();
+                }
+                return $data;
+            } else if ($result['TIPO_USUARIO'] == 'Cliente' and password_verify($datos['password'], $result['CLAVE_USUARIO'])) {
+                try {
+                    $cliente = $this->db->connect()->prepare("SELECT u.ID_USUARIO, u.NOMBRE_USUARIO, u.TIPO_USUARIO, c.ID_CLIENTE, c.NOMBRE_CLIENTE, c.APELLIDO_CLIENTE, u.CLAVE_USUARIO FROM usuario as u INNER JOIN cliente as c ON u.ID_USUARIO = c.ID_USUARIO WHERE u.NOMBRE_USUARIO = :nombre");
+                    $cliente->execute(['nombre' => $datos['user_name']]);
+                    $data = $cliente->fetch(PDO::FETCH_ASSOC);
+                } catch (Exception $e) {
+                    return $e->getMessage();
+                }
+                return $data;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
--- app/models/registromodel.php
+++ app/models/registromodel.php
@@ -1,1 +1,23 @@
-<?php class RegistroModel extends Model{public function __construct(){parent::__construct();}public function insert($datos){try{$connect=$this->db->connect();$queryUsuario=$connect->prepare("INSERT INTO usuario (NOMBRE_USUARIO,CLAVE_USUARIO,TIPO_USUARIO) VALUES(:nombre, :pass, :tipo)");$queryUsuario->execute(['nombre'=>$datos['email'],'pass'=>$datos['pass'],'tipo'=>'1']);$id=$connect->lastInsertId();$queryCliente=$connect->prepare("INSERT INTO cliente (NOMBRE_CLIENTE,APELLIDO_CLIENTE,EDAD,EMAIL_CLIENTE,CELULAR_CLIENTE,ID_USUARIO) VALUES(:nombre, :apellido, :edad, :email, :cel, :id)");$queryCliente->execute(['nombre'=>$datos['nombre'],'apellido'=>$datos['apellido'],'edad'=>$datos['edad'],'email'=>$datos['email'],'cel'=>$datos['cel'],'id'=>$id]);return true;}catch(Exception $e){return false;}}} ?>
+<?php class RegistroModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function insert($datos)
+    {
+        $password_hash = password_hash($datos['pass'], PASSWORD_BCRYPT);
+        
+        try {
+            $connect = $this->db->connect();
+            $queryUsuario = $connect->prepare("INSERT INTO usuario (NOMBRE_USUARIO,CLAVE_USUARIO,TIPO_USUARIO) VALUES(:nombre, :pass, :tipo)");
+            $queryUsuario->execute(['nombre' => $datos['email'], 'pass' => $password_hash, 'tipo' => '1']);
+            $id = $connect->lastInsertId();
+            $queryCliente = $connect->prepare("INSERT INTO cliente (NOMBRE_CLIENTE,APELLIDO_CLIENTE,EDAD,EMAIL_CLIENTE,CELULAR_CLIENTE,ID_USUARIO) VALUES(:nombre, :apellido, :edad, :email, :cel, :id)");
+            $queryCliente->execute(['nombre' => $datos['nombre'], 'apellido' => $datos['apellido'], 'edad' => $datos['edad'], 'email' => $datos['email'], 'cel' => $datos['cel'], 'id' => $id]);
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+}
