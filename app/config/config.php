eed04638cb1285a6fac3c8aef184a9f6f5074523
Dasacav3 <dscv3719@gmail.com>
2021-12-14 20:05:45 -0500

    Updated constants values to env variables

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,15 @@
 <?php
 
-    define('URL', 'http://localhost/cinemax/');
+require __DIR__ . '/../../vendor/autoload.php';
 
-    define('HOST', 'localhost');
-    define('DB', 'cinemax');
-    define('USER', 'root');
-    define('PASSWORD', 'mariadb');
-    define('CHARSET', 'utf8');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
+$dotenv->load();
+
+define('URL', $_ENV['APP_URL']);
+
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_CHARSET', $_ENV['DB_CHARSET']);
+define('DB_PORT', $_ENV['DB_PORT']);
